b5c9727bd08b51d7ad04afb31ea440145cb174d4
张文旭 <zhangwenxu@163.com>
2019-10-08 09:27:33 +0800

    graceui 框架

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App'
 
+
+
 Vue.config.productionTip = false
 Vue.prototype.$authLoginUrl = './api/authenticate'
 Vue.prototype.$accountUrl = './api/account'
--- pages/courseDetails/courseDetails/courseDetails.vue
+++ pages/courseDetails/courseDetails/courseDetails.vue
@@ -1,19 +1,29 @@
 <template>
-	<view>
-		
-	</view>
+  <view class="grace-tab">
+      <scroll-view class="grace-tab-title" scroll-x="true">
+          <view>选项卡标题</view>
+          <view>选项卡标题</view>
+      </scroll-view>
+      <swiper class="grace-tab-swiper" style="height:350rpx;">
+          <swiper-item>对应的内容区</swiper-item>
+      </swiper>
+  </view>
 </template>
-
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
-		}
-	}
+	
+  export default {
+    data() {
+      return {
+        activeName: 'second'
+      };
+    },
+    methods: {
+      handleClick(tab, event) {
+        console.log(tab, event);
+      }
+    }
+  };
 </script>
-
 <style>
 
 </style>
