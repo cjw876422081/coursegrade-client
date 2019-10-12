ca902c3655e28f6955f9843b7031a88d169ceb2a
顾林杰 <2465105167@qq.com>
2019-10-12 14:49:46 +0800

    创建班级修改

--- pages/createGroup/createGroup.vue
+++ pages/createGroup/createGroup.vue
@@ -52,14 +52,14 @@
 			formSubmit(e){
 				console.log("group formSubmit",e);
 				const formData=e.detail.value;
-				//const validResult=this.formValid(formData);
-				// if(!validResult){
-				// 	uni.showToast({
-				// 		icon:'none',
-				// 		title:"输入数据不合法"
-				// 	});
-				// 	return;
-				// }
+				const validResult=this.formValid(formData);
+				if(!validResult){
+					uni.showToast({
+						icon:'none',
+						title:"输入数据不合法"
+					});
+					return;
+				}
 				this.coursegroupService.getCreateGroup(formData,5).then((result)=>{
 					console.log("group formSubmit callback",result);
 					if(result.data && result.data.courseGroupId>0){
@@ -79,26 +79,6 @@
 					})
 					
 				},
-				
-				// this.groupService.createGroup(formData).then((result)=>{
-				// 		console.log("course formSubmit callback",result);
-				// 		if(result.data && result.data.id>0){
-				// 			uni.showToast({
-				// 			    icon:'success',
-				// 			    title: "班级创建成功"
-				// 			});
-				// 			setTimeout(()=>{
-				// 				console.log("settime out")
-				// 				uni.navigateBack();
-				// 			},1500);
-				// 		}
-				// 	}).catch((error)=>{
-						
-				// 	}).finally(()=>{
-						
-				// 	})
-					
-				// },
 				formValid(formData){
 					this.groupCodeError=false;
 					this.groupNameError=false;
@@ -107,13 +87,13 @@
 						error:false
 					}
 					let result=true;
-					if(formData.courseGroupId==null || formData.courseGroupId.length==0){
+					if(formData.groupCode==null || formData.groupCode.length==0){
 						errItem.error=true;
 						errItem.errorText="请输入班级号";
 						this.groupCodeError=true;
 						result= false;
 					}
-					if(formData.courseGroupName==null || formData.courseGroupName.length==0){
+					if(formData.groupName==null || formData.groupName.length==0){
 						errItem.error=true;
 						errItem.errorText="请输入班级名称";
 						this.groupNameError=true;
